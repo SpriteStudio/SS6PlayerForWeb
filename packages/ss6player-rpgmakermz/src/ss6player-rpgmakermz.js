76c5be4546a7e347c19a8378de0493b2dd7ad3c2
Naruto TAKAHASHI <tnaruto@gmail.com>
2020-09-04 18:08:50 +0900

    update comment

--- packages/ss6player-rpgmakermz/src/ss6player-rpgmakermz.js
+++ packages/ss6player-rpgmakermz/src/ss6player-rpgmakermz.js
@@ -1,9 +1,11 @@
 
 /*:
  * @target MZ
- *
+ * @plugindesc RPG ツクール MZ で動作する SS6Player プラグインです。
+ * @author Web Technology
+ * @help
+ * @url https://github.com/SpriteStudio/SS6PlayerForWeb
  */
-
 import { SS6Player, SS6Project } from 'ss6player-pixi';
 
 const pluginName = "SS6Player"
