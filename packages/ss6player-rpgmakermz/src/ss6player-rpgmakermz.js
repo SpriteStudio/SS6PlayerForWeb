0521b8812bec0d7cc435aaf7b911ce0751b748e9
Naruto TAKAHASHI <tnaruto@gmail.com>
2020-09-04 17:14:24 +0900

    add ss6player-rpgmakermz package template

---
+++ packages/ss6player-rpgmakermz/src/ss6player-rpgmakermz.js
@@ -0,0 +1,13 @@
+
+/*:
+ * @target MZ
+ *
+ */
+
+import { SS6Player, SS6Project } from 'ss6player-pixi';
+
+const pluginName = "SS6Player"
+
+PluginManager.registerCommand(pluginName, "set", args => {
+  let player = new SS6Project("", null);
+});
