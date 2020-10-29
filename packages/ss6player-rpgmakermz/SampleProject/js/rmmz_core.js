78b96899206492dd4e7d3ae771f15c502e0ae4db
Naruto TAKAHASHI <tnaruto@gmail.com>
2020-10-29 23:05:28 +0900

    update core script from 1.0.2 to 1.1.0

--- packages/ss6player-rpgmakermz/SampleProject/js/rmmz_core.js
+++ packages/ss6player-rpgmakermz/SampleProject/js/rmmz_core.js
@@ -1,5 +1,5 @@
 //=============================================================================
-// rmmz_core.js v1.0.2
+// rmmz_core.js v1.1.0
 //=============================================================================
 
 //-----------------------------------------------------------------------------
@@ -192,7 +192,7 @@ Utils.RPGMAKER_NAME = "MZ";
  * @type string
  * @constant
  */
-Utils.RPGMAKER_VERSION = "1.0.2";
+Utils.RPGMAKER_VERSION = "1.1.0";
 
 /**
  * Checks whether the current RPG Maker version is greater than or equal to
