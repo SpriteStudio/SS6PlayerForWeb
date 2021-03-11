7184a4f1e255e7a19ea2fa0a7a2724d6d07ac312
Naruto TAKAHASHI <tnaruto@gmail.com>
2021-03-11 21:43:09 +0900

    use XMLHttpRequest instead of fetch for support IE 11

--- packages/ss6player-viewer/src/Input/SspkgLoader.ts
+++ packages/ss6player-viewer/src/Input/SspkgLoader.ts
@@ -4,17 +4,23 @@ export class SspkgLoader {
   load(url: string, onFinishCallback: (ssfbFileData: Uint8Array, imageBinaryMap: { [key: string]: Uint8Array; }, error: any) => void) {
     const self = this;
 
-    fetch(url)
-      .then(function (response: Response) {
-        if (response.status === 200 || response.status === 0) {
-          return Promise.resolve(response.blob());
-        } else {
-          return Promise.reject(new Error(response.statusText));
-        }
-      })
-      .then(JSZip.loadAsync)
-      .then(function (zipFile: JSZip) {
-        console.log(zipFile);
+    const httpObj = new XMLHttpRequest();
+    const method = 'GET';
+    httpObj.open(method, url, true);
+    httpObj.responseType = 'arraybuffer';
+    httpObj.onload = function () {
+      if (!(httpObj.status === 200 || httpObj.status === 0)) {
+        // failed
+        onFinishCallback(null, null, new Error(this.statusText));
+      }
+      const arrayBuffer = this.response;
+      const bytes = new Uint8Array(arrayBuffer);
+
+      let zip = new JSZip();
+      zip.loadAsync(bytes, {
+        checkCRC32: true
+      }).then(function (zipFile: JSZip) {
+        // console.log(zipFile);
 
         let ssfbFilePath = null;
         const imageBinaryMap = {};
@@ -22,12 +28,13 @@ export class SspkgLoader {
         for (let fileName in zipFile.files) {
           const file = zipFile.files[fileName];
           const fileExtension = fileName.split('.').pop();
-          console.log(fileName, file, fileExtension);
+          // console.log(fileName, file, fileExtension);
 
           if (fileExtension === 'ssfb') {
             if (ssfbFilePath !== null) {
               // 既に ssfb が存在していた場合、エラー
-
+              onFinishCallback(null, null, new Error("already exist ssfb file"));
+              return;
             }
             ssfbFilePath = fileName;
           } else if (fileExtension === 'png') {
@@ -43,9 +50,20 @@ export class SspkgLoader {
         zipFile.file(ssfbFilePath).async('uint8array').then((uint8Array: Uint8Array) => {
           onFinishCallback(uint8Array, imageBinaryMap, null);
         });
-      }, function error(e) {
-        console.log(e);
+      }, function (e) {
+        // Error: Corrupted zip : CRC32 mismatch
         onFinishCallback(null, null, e);
       });
+    };
+
+    httpObj.ontimeout = function () {
+      // timeout
+      onFinishCallback(null, null, new Error("timeout"));
+    };
+    httpObj.onerror = function () {
+      // error
+      onFinishCallback(null, null, new Error("error"));
+    };
+    httpObj.send(null);
   }
 }
