0248cfe7811fdc3e7505f02879b185a40faaf989
Naruto TAKAHASHI <tnaruto@gmail.com>
2022-03-18 21:55:19 +0900

    update sampleapp rpgmaker core script from 1.2.0 to 1.4.4.

--- packages/ss6player-rpgmakermz/SampleProject/js/rmmz_scenes.js
+++ packages/ss6player-rpgmakermz/SampleProject/js/rmmz_scenes.js
@@ -1,5 +1,5 @@
 //=============================================================================
-// rmmz_scenes.js v1.3.2
+// rmmz_scenes.js v1.4.4
 //=============================================================================
 
 //-----------------------------------------------------------------------------
@@ -1588,6 +1588,7 @@ Scene_Item.prototype.createItemWindow = function() {
     if (!this._categoryWindow.needsSelection()) {
         this._itemWindow.y -= this._categoryWindow.height;
         this._itemWindow.height += this._categoryWindow.height;
+        this._itemWindow.createContents();
         this._categoryWindow.update();
         this._categoryWindow.hide();
         this._categoryWindow.deactivate();
@@ -2350,7 +2351,8 @@ Scene_Load.prototype.reloadMapIfUpdated = function() {
         const mapId = $gameMap.mapId();
         const x = $gamePlayer.x;
         const y = $gamePlayer.y;
-        $gamePlayer.reserveTransfer(mapId, x, y);
+        const d = $gamePlayer.direction();
+        $gamePlayer.reserveTransfer(mapId, x, y, d, 0);
         $gamePlayer.requestMapReload();
     }
 };
